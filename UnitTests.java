80b70092c890a2c9abac4df564b23e688d807a80
Rodrigo Junqueira <junkeira.rodri@protonmail.com>
2024-12-16 10:06:11 +0000

    class tests

---
+++ UnitTests.java
@@ -0,0 +1,139 @@
+public class UnitTests {
+
+    public static boolean testMatrixAdd(double[][] matrix1, double[][] matrix2, double[][] expected)
+	{
+		matrix1 = matrixAdd(matrix1, matrix2);
+		return (matrixEquals(matrix1, expected));
+	}
+
+	public static boolean testMatrixMulti(double[][] matrix1, double[][] matrix2, double[][] expected)
+	{
+		matrix1 = matrixMulti(matrix1, matrix2);
+		return (matrixEquals(matrix1, expected));
+	}
+
+	public static boolean testMatrixDivConst(double[][] matrix1, double[][] expected, int value)
+	{
+
+		matrix1 = matrixMultiConst(matrix1, value);
+		return (matrixEquals(matrix1, expected));
+	}
+
+	public static boolean testMatrixMultiConst(double[][] matrix1, double[][] expected, int value)
+	{
+
+		matrix1 = matrixMultiConst(matrix1, value);
+		return (matrixEquals(matrix1, expected));
+
+	}
+
+	public static boolean testMatrixTranspose(double[][] matrix1,  double[][] expected)
+	{
+		matrix1 = matrixTranspose(matrix1);
+		return (matrixEquals(matrix1, expected));
+	}
+
+	public static boolean testVectorAdd(double[] vector1, double[] vector2, double[] expected)
+	{
+		vector1 = vectorAdd(vector1, vector2);
+		return (vectorEquals(vector1,expected));
+	}
+
+	public static boolean testVectorMulti(double[] vector1, double[] vector2, double expected)
+	{
+		double result = vectorMulti(vector1, vector2);
+		
+		if(result == expected)
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean testVectorDivConst(double[] vector1, double[] expected, int value)
+	{
+		double[] result = vectorMultConst(vector1, value);
+		return (vectorEquals(result,expected));
+	}
+
+	public static boolean testVectorMultiConst(double[] vector1, double[] expected, int value)
+	{
+		double[] result = vectorMultConst(vector1, 1);
+		return (vectorEquals(result,expected));
+	}
+
+	public static void testCSVtoMatrix(String filename)
+	{
+		double[][]	matrix;
+
+		matrix = CSVtoMatrix("test.csv");
+
+		printMatrix(matrix);
+	}
+
+	public static void testReadingCsv(String filename)
+	{
+		double[][] matrix = ReadingCsv("test.csv");
+
+		printMatrix(matrix);
+	}
+
+	public static void testReadingDir(String dirPath){
+
+		String[] csvFiles = ReadingDir("db");
+
+		for(String file : csvFiles){
+			System.out.printf("%s ", file);
+		}
+	}
+
+	public static void testTransformDoubleMatrixToRealMatrix(double[][] matrix)
+	{
+		RealMatrix result = transformDoubleMatrixToRealMatrix(matrix);
+
+		for(int i = 0; i < result.getRowDimension(); i++){
+			for(int j = 0; j < result.getColumnDimension(); j++){
+				System.out.printf("%.3f ", result.getEntry(i, j));
+			}
+			System.out.println();
+		}
+	}
+
+	public static boolean testIsValueInArray() {
+		
+		int[] array = {1, 2, 3};
+
+		if(isValueInArray(2, array))
+			return true;
+		else	
+			return false;
+		
+	}
+
+	public static boolean testGetCoordinatesOfMinValuesOfDiagonalMatrix(double[][] matrix, int NumberOfvalues, int[] expected)
+	{
+
+		int[] result = getCoordinatesOfMinValuesOfDiagonalMatrix(matrix, 2);
+
+		for(int i = 0; i < NumberOfvalues; i++){
+			if(result[i] != expected[i]){
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public static boolean testGetEigenValuesSubMatrix(int[] arrayOfCoordinates, double[][] matrix, double[][] expected)
+	{
+		double[][] result = getEigenValuesSubMatrix(arrayOfCoordinates, matrix);
+
+		return matrixEquals(result, expected);
+	}
+
+	public static boolean  testGetEigenVectorsSubMatrix(int[] arrayOfCoordinates, double[][] matrix, double[][] expected){
+		double[][] result = getEigenVectorsSubMatrix(arrayOfCoordinates, matrix);
+
+		return matrixEquals(result, matrix);
+	}
+    
+}
